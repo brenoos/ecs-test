796a71b9fd01db155816eea08033e64c0619f1b8
brenoos <breno.oliver07@gmail.com>
2019-05-30 19:18:41 -0300

    parte 4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {    res.send('Nova versão by buddy! part 3')})
+app.get('/', (req, res) => {    res.send('Nova versão by buddy! part 4')})
 
 app.listen(3000, () => {    console.log('Server is up on 3000')})
