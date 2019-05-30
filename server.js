3e767b1531cfd61914d82bfa75adeed7bc6a3ccc
brenoos <breno.oliver07@gmail.com>
2019-05-30 14:51:30 -0300

    commit buddy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {    res.send('Hello world from a Node.js app!')})
+app.get('/', (req, res) => {    res.send('Nova versão by buddy!')})
 
 app.listen(3000, () => {    console.log('Server is up on 3000')})
